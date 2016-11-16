8e113c92e7a9033d3f51a8a286edae5c317b21fc
Eric Berquist <eric.berquist@gmail.com>
2016-11-16 10:16:21 -0500

    C++: Armadillo identity matrix

---
+++ cpp/armadillo/arma_eye.cpp
@@ -0,0 +1,25 @@
+#include <armadillo>
+
+int main()
+{
+
+    size_t dim = 5;
+    double fl = 0.567;
+
+    arma::mat em = arma::eye<arma::mat>(dim, dim);
+    em.print("em");
+
+    // type is arma::SizeMat?
+    // auto s = arma::size(em);
+    // s.print("s");
+
+    arma::mat em2 = arma::eye<arma::mat>(arma::size(em));
+    em2.print("em2");
+
+    arma::mat em3 = fl * arma::eye<arma::mat>(arma::size(em));
+    em3.print("em3");
+
+    return 0;
+
+}
+    
