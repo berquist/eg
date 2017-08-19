8c351d680ad026660fea9924f05e85ac5cc73595
Eric Berquist <eric.berquist@gmail.com>
2017-08-19 11:27:55 -0400

    [C++] matrix-vector elementwise product

---
+++ cpp/armadillo/schur_matrix_vector.cpp
@@ -0,0 +1,34 @@
+#include <armadillo>
+
+int main() {
+
+    const size_t dim = 3;
+
+    arma::mat A(dim, dim, arma::fill::ones);
+    A.print("A");
+    arma::mat B = A;
+    arma::vec b(dim);
+    for (size_t i = 0; i < dim; i++)
+        b(i) = (i + 2);
+    b.print("b");
+
+    // error: element-wise multiplication: incompatible matrix dimensions: 3x3 and 3x1
+    // (A % b).print("A % b");
+
+    // This seems silly but it's ok from a memory access standpoint.
+    for (size_t j = 0; j < dim; j++)
+        A.col(j) *= b(j);
+    A.print("% (1)");
+
+    // error: no match for ‘operator*=’ (operand types are ‘arma::subview_row<double>’ and ‘arma::vec {aka arma::Col<double>}’)
+    // for (size_t i = 0; i < dim; i++)
+    //     B.row(i) *= b;
+    // error: element-wise multiplication: incompatible matrix dimensions: 1x3 and 3x1
+    // for (size_t i = 0; i < dim; i++)
+    //     B.row(i) = B.row(i) % b;
+    // This is matrix multiplication!
+    // B *= b;
+    B.print("% (2)");
+
+    return 0;
+}
---
+++ cpp/armadillo/schur_matrix_vector.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+dim = 3
+
+A = np.ones(shape=(dim, dim))
+B = A.copy()
+b = np.empty(dim)
+for i in range(dim):
+    b[i] = i + 2
+
+print('A')
+print(A)
+print('b')
+print(b)
+
+for j in range(dim):
+    A[:, j] *= b[j]
+print('% (1)')
+print(A)
+
+print('% (2)')
+print(B * b)
