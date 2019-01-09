c79c768e25d9a1af29920e817e4c12519f7087da
Eric Berquist <berquist@isi.edu>
2019-01-09 16:11:04 -0500

    [Python] example of too small input for convolution kernel size in pytorch

---
+++ python/pytorch_bad_conv_size.py
@@ -0,0 +1,8 @@
+import torch
+layer = torch.nn.Conv1d(16, 100, kernel_size=(5,), stride=(1,))
+input_ok = torch.randn(294, 16, 7)
+input_bad = torch.randn(470, 16, 4)
+output_ok = layer(input_ok)
+output_bad = layer(input_bad)
+print(output_ok.size())
+print(output_bad.size())
