8c21a9d5d5a5e8417792ebb1bd4e266d78310b9b
Eric Berquist <eric.berquist@gmail.com>
2019-07-05 14:01:52 -0400

    [Fortran 77] different ways of calling f2py-wrapped functions

--- linking/f77/mylib.f
+++ linking/f77/mylib.f
@@ -12,3 +12,18 @@ Cf2py double precision intent(in,out), depend(n) :: y(n)
          y(i) = alpha * x(i) + y(i)
       enddo
       end subroutine
+
+      subroutine daxpy_inplace(n, alpha, x, y)
+      implicit none
+      integer n
+      double precision alpha
+      double precision x(n), y(n)
+Cf2py integer intent(in) :: n
+Cf2py double precision intent(in) :: alpha
+Cf2py double precision intent(in), depend(n) :: x(n)
+Cf2py double precision intent(inout), depend(n) :: y(n)
+      integer i
+      do i = 1, n
+         y(i) = alpha * x(i) + y(i)
+      enddo
+      end subroutine
--- linking/f77/using_f2py.py
+++ linking/f77/using_f2py.py
@@ -1,16 +1,24 @@
-from mylib_f77 import daxpy
+import numpy as np
+
+import mylib_f77
 
 
 if __name__ == "__main__":
-
-    print(daxpy)
 
     x = [2.0, 3.0, 4.0]
     y = [1.0, 1.0, 1.0]
     alpha = 1.0
     n = len(x)
-    ret = daxpy(n, alpha, x, y)
-    print(ret)
-    print(type(ret))
-    print(y)
-    # assert list(y) == [1.0, 1.0, 1.0]
+    # Functions can take iterables, but you get an ndarray back. You also
+    # modify an argument inplace if it isn't an ndarray.
+    ret = mylib_f77.daxpy(n, alpha, x, y)
+    assert y == [1.0, 1.0, 1.0]
+    assert isinstance(ret, np.ndarray)
+    ref = np.array([3.0, 4.0, 5.0])
+    np.testing.assert_equal(ret, ref)
+
+    # switch from intent(in,out) to intent(inout)
+    y = np.array([1.0, 1.0, 1.0])
+    ret = mylib_f77.daxpy_inplace(n, alpha, x, y)
+    np.testing.assert_equal(y, ref)
+    assert ret == None
