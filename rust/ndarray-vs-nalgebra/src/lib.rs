b7b1e3927e26f547547039a3c09fdcc68528eadb
Eric Berquist <eric.berquist@gmail.com>
2020-06-02 23:28:09 -0400

    [Rust] fix add1 examples

--- rust/ndarray-vs-nalgebra/src/lib.rs
+++ rust/ndarray-vs-nalgebra/src/lib.rs
@@ -9,24 +9,7 @@ pub fn add1_ret(m: &Array<f64, Ix2>) -> Array<f64, Ix2> {
 
 pub fn add1_part_mut(m: &mut Array<f64, Ix2>) {
     let shape = m.shape();
-    let mut ones: Array<f64, _> = Array::ones((shape[0], shape[1]));
-    // m += ones;
-    // m = m + &mut ones;
-    // m = &mut (ones + m);
-    // m = &mut ones;
-
-    // WHY DOESN'T THIS WORK
-    // m = &mut ones + m;
-
-    // m.fill(2.0);
-
-    // let mc = m.clone();
-    // m = &mut (mc + ones);
-
-    // *m = m + ones;
-    // let vm = m.view();
-    // vm = vm + ones.view();
-    // println!("{}", vm);
+    *m += &Array::ones((shape[0], shape[1]));
 }
 
 pub fn perform_add_nalgebra(a: &Matrix4<u64>, b: &Matrix4<u64>) -> Matrix4<u64> {
@@ -64,16 +47,12 @@ mod tests {
     #[test]
     fn it_works() {
         let dim = 10;
-        // let m1: Array<f64, _> = Array::ones((dim, dim));
-        // println!("{:?}", add1_ret(&m1));
+        let m1: Array<f64, _> = Array::ones((dim, dim));
+        println!("m1: {:?}", add1_ret(&m1));
 
         let mut m2: Array<f64, _> = Array::ones((dim, dim));
-        println!("{:?}", m2);
+        println!("m2 before: {:?}", m2);
         add1_part_mut(&mut m2);
-        println!("{:?}", m2);
-
-        // let cm3 = RefCell::new(Array::ones((dim, dim)));
-        // add_1_refcell(&cm3);
-        // println!("{:?}", cm3);
+        println!("m2 after: {:?}", m2);
     }
 }
