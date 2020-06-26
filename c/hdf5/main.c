70e1233a6f10135a944217db68a647fe87be19ca
Eric Berquist <eric.berquist@gmail.com>
2020-06-25 20:05:28 -0400

    [C] 'deleting' from an HDF5 file

--- c/hdf5/main.c
+++ c/hdf5/main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <hdf5.h>
 
@@ -13,9 +14,28 @@ int main() {
     dataspace_id = H5Screate_simple(rank, dims, NULL);
     hid_t dataset_id;
     dataset_id = H5Dcreate(file_id, "/dset", H5T_STD_I32BE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+    htri_t exists;
+    /* Returns:
+       - a positive value if the link exists
+       - 0 if the link does not exist
+       - a negative value when either
+         - the function fails
+         - the link does not exist
+     */
+    exists = H5Lexists(file_id, "/dset", H5P_DEFAULT);
+    assert(exists > 0);
+    printf("exists: %d\n", exists);
+    status = H5Ldelete(file_id, "/dset", H5P_DEFAULT);
+    exists = H5Lexists(file_id, "/dset", H5P_DEFAULT);
+    assert(exists <= 0);
+    printf("exists: %d\n", exists);
+    printf("%d\n", status);
     status = H5Dclose(dataset_id);
+    printf("%d\n", status);
     status = H5Sclose(dataspace_id);
+    printf("%d\n", status);
     status = H5Fclose(file_id);
+    printf("%d\n", status);
     free(dims);
     return 0;
 }
