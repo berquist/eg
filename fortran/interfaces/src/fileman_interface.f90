51daf704d15ecbee19f13f26755b51aa7a92ed0d
Eric Berquist <eric.berquist@gmail.com>
2020-08-19 10:51:06 -0400

    [Fortran] add intents to interface

--- fortran/interfaces/src/fileman_interface.f90
+++ fortran/interfaces/src/fileman_interface.f90
@@ -5,22 +5,23 @@ module fileman_module
   interface fileman
 
      subroutine fileman_byte(oper, filenum, datatype, length, offset, offtype, data)
-       integer :: oper, filenum, datatype, length, offset, offtype
-       character :: data
+       integer, intent(in) :: oper, filenum, datatype, length, offset, offtype
+       character, intent(inout) :: data
      end subroutine fileman_byte
 
      subroutine fileman_dp(oper, filenum, datatype, length, offset, offtype, data)
-       integer :: oper, filenum, datatype, length, offset, offtype
-       real (kind=8) :: data
+       integer, intent(in) :: oper, filenum, datatype, length, offset, offtype
+       real (kind=8), intent(inout) :: data
      end subroutine fileman_dp
 
      subroutine fileman_int(oper, filenum, datatype, length, offset, offtype, data)
-       integer :: oper, filenum, datatype, length, offset, offtype, data
+       integer, intent(in) :: oper, filenum, datatype, length, offset, offtype
+       integer, intent(inout) :: data
      end subroutine fileman_int
 
      subroutine fileman_lgcl(oper, filenum, datatype, length, offset, offtype, data)
-       integer :: oper, filenum, datatype, length, offset, offtype
-       logical :: data
+       integer, intent(in) :: oper, filenum, datatype, length, offset, offtype
+       logical, intent(inout) :: data
      end subroutine fileman_lgcl
 
   end interface fileman
