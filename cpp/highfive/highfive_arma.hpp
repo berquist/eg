4ad69d24b361e34a5950925d50513cea5f5a317e
Eric Berquist <eric.berquist@gmail.com>
2021-11-08 14:00:10 -0800

    [C++] Armadillo with HighFive: place inside namespaces like existing HighFive code

--- cpp/highfive/highfive_arma.hpp
+++ cpp/highfive/highfive_arma.hpp
@@ -1,15 +1,19 @@
+#ifndef LIBSTORE_IMPL_HDF5_HIGHFIVE_ARMA_H_
+#define LIBSTORE_IMPL_HDF5_HIGHFIVE_ARMA_H_
+
 #include <highfive/H5File.hpp>
 #include <armadillo>
 
-using namespace HighFive;
+namespace HighFive {
+namespace details {
 
 //// Col<T>
 
 template <typename T>
-struct details::inspector<arma::Col<T>> {
+struct inspector<arma::Col<T>> {
     using type = arma::Col<T>;
     using value_type = T;
-    using base_type = typename details::inspector<value_type>::base_type;
+    using base_type = typename inspector<value_type>::base_type;
 
     static const size_t ndim = 2;
     static const size_t recursive_ndim = ndim + inspector<value_type>::recursive_ndim;
@@ -21,14 +25,14 @@ struct details::inspector<arma::Col<T>> {
 };
 
 template <typename T>
-struct details::data_converter<arma::Col<T>>
-    : public details::container_converter<arma::Col<T>> {
+struct data_converter<arma::Col<T>>
+    : public container_converter<arma::Col<T>> {
 
     using container_type = arma::Col<T>;
-    using value_type = typename details::inspector<T>::base_type;
+    using value_type = typename inspector<T>::base_type;
 
     inline data_converter(const DataSpace &space)
-        : details::container_converter<container_type>(space), _space(space) {
+        : container_converter<container_type>(space), _space(space) {
 
         const std::vector<size_t> dims = _space.getDimensions();
         assert(dims.size() == 2);
@@ -51,10 +55,10 @@ struct details::data_converter<arma::Col<T>>
 //// Row<T>
 
 template <typename T>
-struct details::inspector<arma::Row<T>> {
+struct inspector<arma::Row<T>> {
     using type = arma::Row<T>;
     using value_type = T;
-    using base_type = typename details::inspector<value_type>::base_type;
+    using base_type = typename inspector<value_type>::base_type;
 
     static const size_t ndim = 2;
     static const size_t recursive_ndim = ndim + inspector<value_type>::recursive_ndim;
@@ -66,14 +70,14 @@ struct details::inspector<arma::Row<T>> {
 };
 
 template <typename T>
-struct details::data_converter<arma::Row<T>>
-    : public details::container_converter<arma::Row<T>> {
+struct data_converter<arma::Row<T>>
+    : public container_converter<arma::Row<T>> {
 
     using container_type = arma::Row<T>;
-    using value_type = typename details::inspector<T>::base_type;
+    using value_type = typename inspector<T>::base_type;
 
     inline data_converter(const DataSpace &space)
-        : details::container_converter<container_type>(space), _space(space) {
+        : container_converter<container_type>(space), _space(space) {
 
         const std::vector<size_t> dims = _space.getDimensions();
         assert(dims.size() == 2);
@@ -96,10 +100,10 @@ struct details::data_converter<arma::Row<T>>
 //// Mat<T>
 
 template <typename T>
-struct details::inspector<arma::Mat<T>> {
+struct inspector<arma::Mat<T>> {
     using type = arma::Mat<T>;
     using value_type = T;
-    using base_type = typename details::inspector<value_type>::base_type;
+    using base_type = typename inspector<value_type>::base_type;
 
     static const size_t ndim = 2;
     static const size_t recursive_ndim = ndim + inspector<value_type>::recursive_ndim;
@@ -111,10 +115,10 @@ struct details::inspector<arma::Mat<T>> {
 };
 
 template <typename T>
-struct details::data_converter<arma::Mat<T>> {
+struct data_converter<arma::Mat<T>> {
 
     using container_type = arma::Mat<T>;
-    using value_type = typename details::inspector<T>::base_type;
+    using value_type = typename inspector<T>::base_type;
 
     inline data_converter(const DataSpace &space)
         : _space(space) {
@@ -147,26 +151,25 @@ struct details::data_converter<arma::Mat<T>> {
 //// Cube<T>
 
 template <typename T>
-struct details::inspector<arma::Cube<T>> {
+struct inspector<arma::Cube<T>> {
     using type = arma::Cube<T>;
     using value_type = T;
-    using base_type = typename details::inspector<value_type>::base_type;
+    using base_type = typename inspector<value_type>::base_type;
 
     static const size_t ndim = 3;
     static const size_t recursive_ndim = ndim + inspector<value_type>::recursive_ndim;
 
     static std::array<size_t, recursive_ndim> getDimensions(const type &val) {
-        // Each slice is stored contiguously in memory, so
         std::array<size_t, recursive_ndim> sizes{val.n_slices, val.n_rows, val.n_cols};
         return sizes;
     }
 };
 
 template <typename T>
-struct details::data_converter<arma::Cube<T>> {
+struct data_converter<arma::Cube<T>> {
 
     using container_type = arma::Cube<T>;
-    using value_type = typename details::inspector<T>::base_type;
+    using value_type = typename inspector<T>::base_type;
 
     inline data_converter(const DataSpace &space)
         : _space(space) {
@@ -177,8 +180,6 @@ struct details::data_converter<arma::Cube<T>> {
 
     inline value_type * transform_read(container_type &container) {
         const std::vector<size_t> dims = _space.getDimensions();
-        // container.set_size(dims[1], dims[2], dims[0]);
-
         // The row/col dims need to be flipped here, but the actual data will
         // be flipped in process_result.  We can't do it here because the read
         // happens in row-major order.
@@ -187,16 +188,13 @@ struct details::data_converter<arma::Cube<T>> {
     }
 
     inline const value_type * transform_write(const container_type &container) noexcept {
-        // arma::Col<T> v = arma::vectorise(container);
         _buffer.resize(container.n_elem);
         size_t offset;
         for (size_t k = 0; k < container.n_slices; k++) {
             for (size_t j = 0; j < container.n_cols; j++) {
                 for (size_t i = 0; i < container.n_rows; i++) {
-                    // offset = i + (container.n_rows * j) + (container.n_rows * container.n_cols * k);
                     // Transpose row and column indices
                     offset = j + (container.n_cols * i) + (container.n_rows * container.n_cols * k);
-                    // std::cout << "(" << i << "," << j << "," << k << ") [" << offset << "] = " << container(i, j, k) << " [" << v[offset] << "]" << std::endl;
                     _buffer[offset] = container(i, j, k);
                 }
             }
@@ -215,3 +213,8 @@ struct details::data_converter<arma::Cube<T>> {
     const DataSpace& _space;
     std::vector<value_type> _buffer;
 };
+
+} // end namespace details
+} // end namespace HighFive
+
+#endif // LIBSTORE_IMPL_HDF5_HIGHFIVE_ARMA_H_
