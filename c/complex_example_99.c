01e7bf1ec016f0c1925e38c716effdfe53731006
Eric Berquist <eric.berquist@gmail.com>
2022-04-24 18:19:57 -0400

    [C] print real and imaginary parts of complex number in C99

--- c/complex_example_99.c
+++ c/complex_example_99.c
@@ -4,8 +4,9 @@
 int main() {
     /* Taken from
      * https://www.gnu.org/software/gnu-c-manual/gnu-c-manual.html#Complex-Number-Types */
-    complex double z = 1.0 + 3.0*I; 
-    printf("Phase is %f, modulus is %f\n", carg(z), cabs(z));        
+    complex double z = 1.0 + 3.0*I;
+    printf("Phase is %lf, modulus is %lf\n", carg(z), cabs(z));
+    printf("Real part is %lf, imaginary part is %lf\n", creal(z), cimag(z));
 
     return 0;
 }
