4f25293bc02b3c11be2bcf5b3ff0ac5cb41bcd5b
Eric Berquist <eric.berquist@gmail.com>
2022-06-27 16:40:19 -0400

    [Python] saving strings and extending datasets with h5py

---
+++ python/h5py/test_lists_and_strings.py
@@ -0,0 +1,20 @@
+import h5py
+
+
+FNAME = "lists_and_strings.h5"
+
+h5file = h5py.File(FNAME, mode="w", libver="latest")
+
+l1 = [43, 44, 29]
+# h5file["l1"] = l1
+# l1.append(99)
+# h5file["l1"] = l1
+dset_l1 = h5file.create_dataset("l1", data=l1, maxshape=(None,))
+print(dset_l1[()])
+l1.append(99)
+dset_l1.resize((len(l1),))
+dset_l1[()] = l1
+print(dset_l1[()])
+
+l2 = ["apple", "pear", "banana"]
+h5file["l2"] = l2
