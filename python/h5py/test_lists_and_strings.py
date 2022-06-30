9f018479427eab50bbd91c367c72195c07b4cdea
Eric Berquist <eric.berquist@gmail.com>
2022-06-30 17:09:15 -0400

    [Python] can also update lists of strings if dset is chunked

--- python/h5py/test_lists_and_strings.py
+++ python/h5py/test_lists_and_strings.py
@@ -24,3 +24,20 @@ h5file["s1"] = s1
 s2 = "how are you"
 # "scalar datasets cannot be extended"
 # dset_s2 = h5file.create_dataset("s2", data=s2, maxshape=(None,))
+
+# TypeError: Only chunked datasets can be resized
+#
+# l2.append("cherry")
+# h5file["l2"].resize((len(l2),))
+# h5file["l2"][()] = l2
+# print(h5file["l2"][()])
+
+l3 = l2.copy()
+
+dset_l3 = h5file.create_dataset("l3", data=l3, maxshape=(None,))
+print(dset_l3[()])
+
+l3.append("cherry")
+h5file["l3"].resize((len(l3),))
+h5file["l3"][()] = l3
+print(h5file["l3"][()])
