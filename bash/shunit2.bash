927ced54a30e5793b3aa9ee9e466601ae3576f51
Eric Berquist <eric.berquist@gmail.com>
2023-05-16 17:47:05 -0400

    [Bash] extending shunit2 example

--- bash/shunit2.bash
+++ bash/shunit2.bash
@@ -1,17 +1,37 @@
 #!/usr/bin/env bash
+# shellcheck disable=SC2317
 
 set -euo pipefail
 
-SHUNIT_COLOR=none
-
 test_make() {
     make hello
-    echo $?
+    echo "?=$?"
 }
 
 test_make_subshell() {
     (make world)
-    echo $?
+    echo "?=$?"
 }
 
-. /usr/bin/shunit2
+always_false() {
+    false
+}
+
+test_make_fail_in_makefile() {
+    make explicit_fail
+    echo "?=$?"
+    echo "Do we get here?"  # answer: no
+}
+
+test_fail_main_shell() {
+    always_false
+    echo "?=$?"
+}
+
+test_fail_subshell() {
+    (always_false)
+    echo "?=$?"
+}
+
+# shellcheck disable=SC1090
+. "$(command -v shunit2)"
