dc59798aa764e19b48366f2a0f6601d5687a55c4
Eric Berquist <eric.berquist@gmail.com>
2023-06-16 23:11:35 -0400

    [Rust] both of these ways work

--- rust/collect/src/main.rs
+++ rust/collect/src/main.rs
@@ -1,6 +1,22 @@
+const DEFAULT_IGNORES: [&str; 8] = [
+    "RCS",
+    "CVS",
+    "tags",
+    ".git",
+    ".hg",
+    ".bzr",
+    "_darcs",
+    "__pycache__",
+];
+
 fn main() {
     let myarr = [2, 4, 5, 8, 2];
     println!("{:?}", myarr);
     let myvec: Vec<_> = myarr.iter().collect();
     println!("{:?}", myvec);
+
+    let ignores_vec: Vec<_> = DEFAULT_IGNORES.iter().map(|x| String::from(*x)).collect();
+    println!("{:?}", ignores_vec);
+    let ignores_vec: Vec<_> = DEFAULT_IGNORES.iter().map(|&x| String::from(x)).collect();
+    println!("{:?}", ignores_vec);
 }
