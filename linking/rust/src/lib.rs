13ec8b4e58de6ad50dd96eb9237aab854ea5b4bc
Eric Berquist <eric.berquist@gmail.com>
2024-02-06 13:35:06 -0500

    [Rust] cargo update

--- linking/rust/src/lib.rs
+++ linking/rust/src/lib.rs
@@ -1,8 +1,7 @@
 extern crate libc;
-use libc::uint32_t;
 
 #[no_mangle]
-pub extern "C" fn addition(a: uint32_t, b: uint32_t) -> uint32_t {
+pub extern "C" fn addition(a: u32, b: u32) -> u32 {
     a + b
 }
 
