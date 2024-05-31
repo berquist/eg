4836d534e89eb954b4c32c3261a2abef95f053c6
Eric Berquist <eric.berquist@gmail.com>
2024-05-31 14:56:42 -0400

    [Bash] docker build args

---
+++ bash/build_args/build_args_file.py
@@ -0,0 +1,19 @@
+from argparse import ArgumentParser
+from pathlib import Path
+import subprocess as sp
+
+
+parser = ArgumentParser()
+parser.add_argument("fn", type=Path)
+args = parser.parse_args()
+fn = args.fn
+
+lines = ""
+for line in fn.read_text(encoding="utf-8").splitlines():
+    line = line.strip()
+    if not line.startswith("#"):
+        # f"--build-arg=\"{line}\""
+        # To mimic bash implementation rather than append to list then join on string
+        lines = f"{lines} --build-arg=\"{line}\""
+print(lines)
+sp.run(f"podman build --no-cache {lines} -t myimage .", shell=True)
--- bash/build_args/build_args_file.sh
+++ bash/build_args/build_args_file.sh
@@ -9,12 +9,12 @@ converted_line=""
 if [ -f "${args_file}" ]; then
     readarray -t args_lines < "${args_file}"
     for arg_line in "${args_lines[@]}"; do
-        converted_line="${converted_line} $(printf -- "--build-arg=%s" ${arg_line})"
-        echo ${arg_line[0]}
+        if [[ ${arg_line} != \#* ]]; then
+            formatted_arg="$(printf -- '--build-arg=\"%s\"' "${arg_line}")"
+            converted_line="${converted_line} ${formatted_arg}"
+        fi
     done
-    #echo "${converted_line[@]}"
 fi
 
-#echo "podman build ${converted_line[*]} -t myimage ."
-# shellcheck disable=SC2086,SC2048
-#podman build --no-cache ${converted_line[*]} -t myimage .
+cmd="podman build --no-cache ${converted_line[*]} -t myimage ."
+eval "${cmd}"
